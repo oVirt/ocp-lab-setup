cc4d21acd33983d1ff248c7f71f30d7c84851fd4
Michal Skrivanek <michal.skrivanek@redhat.com>
2022-09-02 22:53:41 +0200

    fix nic ordering so provisioning is first

--- 35-add-templates.sh
+++ 35-add-templates.sh
@@ -17,8 +17,8 @@ provisioning_id=$(curl_api /vnicprofiles | grep -B1 "<name>provisioning</name>" 
 for i in master worker; do
     id=$(curl_api "/vms?search=name=$i" | sed -n "s/.*vm href.*id=\"\(.*\)\">/\1/p") 
 
-    curl_api /vms/$id/nics -d "<nic> <name>provisioning</name> <vnic_profile id=\"$provisioning_id\"/> </nic>"
-    curl_api /vms/$id/nics -d "<nic> <name>baremetal</name> <vnic_profile id=\"$baremetal_id\"/> </nic>"
+    curl_api /vms/$id/nics -d "<nic> <name>nic1</name> <vnic_profile id=\"$provisioning_id\"/> </nic>"
+    curl_api /vms/$id/nics -d "<nic> <name>nic2</name> <vnic_profile id=\"$baremetal_id\"/> </nic>"
 
     curl_api /vms/$id/diskattachments -d "<disk_attachment> <bootable>true</bootable> <interface>virtio_scsi</interface> <disk> <format>cow</format> <sparse>true</sparse> <provisioned_size>$DISK_SIZE</provisioned_size> <storage_domains> <storage_domain> <name>data</name> </storage_domain> </storage_domains> </disk> </disk_attachment>"
     sleep 5
