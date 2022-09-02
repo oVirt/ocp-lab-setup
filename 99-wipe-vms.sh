32eac7bb4a3c2d8aea25c354222794c4d4d34345
Michal Skrivanek <michal.skrivanek@redhat.com>
2022-09-02 14:10:27 +0200

    wipe VMs and all local storage

---
+++ 99-wipe-vms.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+#
+. common_funcs
+
+[[ "$HOSTS" ]] || die "No hosts" 
+
+echo "Delete all VMs"
+mastervms=$(curl_api "/vms?search=name=master*" | sed -n "s/.*vm href.*id=\"\(.*\)\">/\1/p")
+workervms=$(curl_api "/vms?search=name=worker*" | sed -n "s/.*vm href.*id=\"\(.*\)\">/\1/p")
+for i in $mastervms $workervms; do
+    curl_api "/vms/${i}" -f -X DELETE
+    [[ "$?" -eq 0 ]] || die "failed to delete VM id $i"
+done
+
+echo "Wipe local storage on hosts"
+for i in $HOSTS; do
+	$SSH $i "
+    wipes=\$(lvdisplay ovirt-local -c | grep -v ovirt-local/pool0 | cut -d: -f1);
+    for i in \$wipes; do wipefs -a \$i; done"
+done
