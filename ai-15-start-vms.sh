d79d52d730e8275eb99639b52a8b46a5d12e0829
Michal Skrivanek <michal.skrivanek@redhat.com>
2022-09-12 19:00:58 +0200

    make runs vlatile so next reboot uses original boot order

--- ai-15-start-vms.sh
+++ ai-15-start-vms.sh
@@ -8,5 +8,5 @@ echo "Start $2 worker VMs"
 for i in $(seq $1 1 $(($1 - 1 + $2))); do
     echo "Start worker-$i"
     id=$(curl_api "/vms?search=name=worker-${i}" | sed -n "s/.*vm href.*id=\"\(.*\)\">/\1/p")
-    curl_api "/vms/$id/start" -d "<action> <vm> <os> <boot> <devices> <device>cdrom</device> </devices> </boot> </os> </vm> </action>"
+    curl_api "/vms/$id/start" -d "<action> <volatile>true</volatile> <vm> <os> <boot> <devices> <device>cdrom</device> </devices> </boot> </os> </vm> </action>"
 done
