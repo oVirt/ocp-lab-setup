c1cdc6de01052ba8a00286541d68c0b4b8c22905
Michal Skrivanek <michal.skrivanek@redhat.com>
2022-09-12 18:47:25 +0200

    ai: simple worker VMs mass start
    so that it can be used in smaller batches of e.g. 50VMs, otherwise AI
    times out

---
+++ ai-15-start-vms.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+. common_funcs
+
+[[ $# -eq 2 ]] || die "$0 <start> <how many>"
+
+echo "Start $2 worker VMs"
+for i in $(seq $1 1 $(($1 - 1 + $2))); do
+    echo "Start worker-$i"
+    id=$(curl_api "/vms?search=name=worker-${i}" | sed -n "s/.*vm href.*id=\"\(.*\)\">/\1/p")
+    curl_api "/vms/$id/start" -d "<action> <vm> <os> <boot> <devices> <device>cdrom</device> </devices> </boot> </os> </vm> </action>"
+done
