e78a9e01719828b8dff6510698615845cc45a6c5
Michal Skrivanek <michal.skrivanek@redhat.com>
2022-09-19 19:15:36 +0200

    50-start-vbmc: adapt to the new mass configuration
    create an inventory file and start only one instance of ovirtbmc.py

--- 50-start-vbmc.sh
+++ 50-start-vbmc.sh
@@ -5,25 +5,22 @@
 
 [[ "$ENGINE" ]] || die "No engine" 
 
-echo "Create addresses"
+echo Clone oVirt vBMC project
+$SSH $ENGINE "[[ -d vbmc ]] || { git clone https://github.com/oVirt/vbmc.git; pip3 install -r vbmc/requirements.txt; }"
+
+echo "Generate IPMI inventory"
 local_addrs=""
 for i in  $(seq 1 1 $MASTERS) $(seq 1 1 $WORKERS); do
     next_ipmi
     local_addrs+="$IPMI_ADDRESS/16,"
+    command echo "worker-$i,$IPMI_ADDRESS,623" >> bmc
 done
+$SCP bmc $ENGINE:vbmc/
+
+echo "Define addresses"
 $SSH $ENGINE "nmcli con mod baremetal +ipv4.addresses \"$local_addrs\""
 reset_ipmi_address
 $SSH $ENGINE "nmcli con up baremetal"
 
 echo "Create vBMCs"
-for i in $(seq 1 1 $MASTERS); do
-    next_ipmi
-    [[ "$(curl_api "/vms?search=name=master-${i}" | wc -l)" -gt 2 ]] || die "VM master-$i missing"
-    $SSH $ENGINE "./ovirtbmc.py --address $IPMI_ADDRESS --engine-fqdn $ENGINE --engine-username admin@internal --engine-password password --vm master-$i" &
-done
-for i in $(seq 1 1 $WORKERS); do
-    next_ipmi
-    [[ "$(curl_api "/vms?search=name=worker-${i}" | wc -l)" -gt 2 ]] || die "VM worker-$i missing"
-    $SSH $ENGINE "./ovirtbmc.py --address $IPMI_ADDRESS --engine-fqdn $ENGINE --engine-username admin@internal --engine-password password --vm worker-$i" &
-done
-wait
+$SSH $ENGINE "vbmc/ovirtbmc.py --vm-inventory vbmc/bmc --engine-fqdn $ENGINE --engine-username admin@internal --engine-password password"
